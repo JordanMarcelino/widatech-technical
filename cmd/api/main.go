1a5d027cd83567d3cf985826d113d029f059418b
Jordan Marcelino <jhordan.marcelino@gmail.com>
2025-01-22 21:43:21 +0700

    init project

--- cmd/api/main.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import "github.com/JordanMarcelino/go-gin-starter/cmd/workers"
-
-func main() {
-	workers.Start()
-}
--- internal/constant/context.go
+++
@@ -1,5 +0,0 @@
-package constant
-
-const (
-	CTX_USER_ID = "ctx-user-id"
-)
