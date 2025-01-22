52fca464cd8804ccd58ef44123fa2272f21007c2
Jordan Marcelino <jhordan.marcelino@gmail.com>
2025-01-22 23:51:52 +0700

    chore: use pointer on invoice product

--- go/internal/entity/invoices.go
+++ go/internal/entity/invoices.go
@@ -1,13 +1,11 @@
 package entity
 
-import "time"
-
 type Invoice struct {
 	InvoiceNo       string
-	InvoiceDate     time.Time
+	InvoiceDate     string
 	CustomerName    string
 	SalesPersonName string
 	PaymentType     string
 	Notes           string
-	Products        []Product
+	Products        []*Product
 }
